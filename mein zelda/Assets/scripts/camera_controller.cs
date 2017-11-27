bbe9fa95fb0993f24254b7578baa0952341c79bc
Daemon <daemon@github.com>
2017-11-27 20:27:21 +0300

    das ist mein zelda
    oh yea

---
+++ mein zelda/Assets/scripts/camera_controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_controller : MonoBehaviour {
+	
+	//[SerializeField]
+	private Transform target;
+    private  Vector3 offset;
+	
+    // Use this for initialization
+	void Start () {
+		target = GameObject.FindWithTag ("Player").GetComponent<Transform>();
+		offset = transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = target.transform.position + offset;
+	}
+}
---
+++ mein zelda/Assets/scripts/game_controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_controller : MonoBehaviour {
+
+	[SerializeField]
+	private int score = 0;
+
+	// Use this for initialization
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+
+	public void addScore(){
+		score++;
+	}
+}
